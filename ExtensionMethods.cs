020e255be7c30266a637543dcc19e8c0d35c2d0e
Ash Jones <74986794+DenseTurd@users.noreply.github.com>
2020-12-04 20:50:04 +0000

    Create ExtensionMethods.cs

---
+++ ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class Extensions
+{
+    public static void PlayerPrefsIncrementInt(string str)
+    {
+        PlayerPrefs.SetInt(str, PlayerPrefs.GetInt(str) + 1);
+    }
+
+    public static void PlayerPrefsSetBool(string str, bool val)
+    {
+            PlayerPrefs.SetInt(str, val ? 1 : 0);
+    }
+
+    public static bool PlayerPrefsGetBool(string str)
+    {
+        return PlayerPrefs.GetInt(str) == 1 ? true : false;
+    }
+
+    public static Vector3 With(this Vector3 original, float? x = null, float? y = null, float? z = null)
+    {
+        return new Vector3(x ?? original.x, y ?? original.y, z ?? original.z);
+    }
+}
